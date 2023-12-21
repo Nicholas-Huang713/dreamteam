f3fc19490a9feb8c8174599428a93f045a2c830f
Nicholas_Huang713 <nhuang713@gmail.com>
2023-12-21 09:06:08 -0800

    navbar changes

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -186,24 +186,23 @@ export default function NavBar() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col">
               {navigation.map((item) => {
                 return (
                   <>
                     {item.show === false ?
                       <></> :
-                      <Disclosure.Button
+                      <Link
                         key={item.name}
-                        as="a"
-                        href={item.href}
+                        to={item.to}
                         className={classNames(
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'block rounded-md px-3 py-2 text-base font-medium'
+                          'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
-                      </Disclosure.Button >
+                      </Link>
                     }
                   </>
                 )
